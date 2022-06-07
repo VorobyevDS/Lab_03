d716871b21a7693d0f949c32658519202b7b35a4
VorobyevDS <VorobyevDS@mpei.ru>
2022-06-07 19:15:30 +0300

    First commit

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 
 vector<double>
-input_numbers(size_t count) {
+input_numbers(istream& in, size_t count) {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++) {
-        cin >> result[i];
+        in >> result[i];
     }
     return result;
 }
@@ -133,7 +133,7 @@ int main() {
     cin >> number_count;
 
     cerr<<"Enter numbers:";
-    const auto numbers = input_numbers(number_count);
+    const auto numbers=input_numbers(cin, number_count);
 
     size_t bin_count;
     cerr << "Enter column count: ";
