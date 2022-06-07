39af1213ca1d43e2e9ae9bc9ed91f76738aa2960
VorobyevDS <VorobyevDS@mpei.ru>
2022-06-08 02:43:33 +0300

    cURL 2

--- main.cpp
+++ main.cpp
@@ -99,9 +99,11 @@ main(int argc, char* argv[]) {
             CURLcode res;
             curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
             res = curl_easy_perform(curl);
+            if(res != CURLE_OK)
+                fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+                exit(1);
             curl_easy_cleanup(curl);
         }
-
         return 0;
     }
 
