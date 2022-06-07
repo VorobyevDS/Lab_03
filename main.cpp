2e4fa2e37134c71ae1ec4434f099f773dd42da48
VorobyevDS <VorobyevDS@mpei.ru>
2022-06-08 02:37:10 +0300

    cURL

--- main.cpp
+++ main.cpp
@@ -89,9 +89,22 @@ show_histogram_text(const auto bins){
 
 }
 
-int main() {
+int
+main(int argc, char* argv[]) {
+    if (argc>1){
+        curl_global_init(CURL_GLOBAL_ALL);
 
-    curl_global_init(CURL_GLOBAL_ALL);
+        CURL *curl = curl_easy_init();
+        if(curl) {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+        }
+
+        return 0;
+    }
+
 
     const auto input = read_input(cin, true);
 
