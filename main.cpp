29b19241f800ddf00944439ba47452c69f9f30a0
VorobyevDS <VorobyevDS@mpei.ru>
2022-06-09 01:48:25 +0300

    Still working

--- main.cpp
+++ main.cpp
@@ -95,11 +95,15 @@ show_histogram_text(const auto bins){
 int
 main(int argc, char* argv[]) {
     if (argc>1){
-        cout<<argc;
-        cout<<" "<<argv[1];
+        CURL *curl = curl_easy_init();
+        if(curl) {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+        }
         return 0;
     }
-    curl_global_init(CURL_GLOBAL_ALL);
     const auto input = read_input(cin, true);
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
